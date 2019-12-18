6c51c1cf826c93f41c5657adbe929bfa731ea5b1
mihmatache <Capucino.It4liano>
2019-12-18 18:36:16 +0200

    Updated install script to use virtual box Updated doc at top Fixed issue where the user would be taken from session when not provided Moved location of where certificates are being created

--- minikube/install/install.sh
+++ minikube/install/install.sh
@@ -1,34 +1,50 @@
 #!/bin/bash
 #
-# This script purpose is to make it easier to install minikube
+# Help install a minikube setup
 #
-# Please take into consideration that you will need to introduce
-# two passwords, the first one is the root password and the second
-# one is the luxoft account pasword.
+# Consider this script an alpha version as not validation for success is present
+#
+# If on a LUXOFT network, provide with -u <LUXOFT USERNAME> parameter.
+#     - You will be prompted to pass the password at runtine
+
 
 
 while getopts u: option
 do
 case "${option}"
 in
-u) USER=${OPTARG};;
+u) LUXUSER=${OPTARG};;
 *) UNKNOWN=${OPTARG};;
 esac
 done
 
 
-if [ -z "$UNKNOWN" ]
-then
+if [ -n "$UNKNOWN" ]; then
+  echo "$UNKNOWN"
+  echo 'Has extra parameter'
   exit 1
 fi
 
+if [ -n "$LUXUSER" ]; then
+  sudo install cpfw-login_amd64 /usr/local/bin/
+  cpfw-login_amd64 --user "$LUXUSER"
+
+  # Create directories and their parent directories if necessary
+  sudo mkdir -p /usr/local/share/ca-certificates/luxoft
+  sudo mkdir -p ~/.minikube/files/etc/ssl/certs
+
+  # Generate certificate
+  echo -n | openssl s_client -showcerts -connect dl.k8s.io:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > kube.chain.pem
+
+  # Extract the last certificate
+  csplit -f htf kube.chain.pem '/-----BEGIN CERTIFICATE-----/' '{*}' | sudo cat $(ls htf* | sort | tail -1) > luxoft_root_ca.crt && rm -rf htf*
+fi
 # Install curl
 sudo apt-get install -y curl
 
 # install kubectl
-
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x ./kubectl
+sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 # Uninstall docker first
@@ -36,6 +52,9 @@ sudo apt-get -y remove docker docker-engine docker.io containerd runc
 
 # Install packaes to allow apt to use a repository over HTTPS
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+#Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Install Docker
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
@@ -50,22 +69,12 @@ sudo chmod +x minikube
 sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
 
+#Install VirtualBox
+sudo apt-get -y update
+sudo apt-get -y install virtualbox
+
 # Add to path
-if [ -z "$USER" ]
-then
-  sudo install cpfw-login_amd64.bin /usr/local/bin/
-  cpfw-login_amd64.bin --user "$USER"
-
-  # Create directories and their parent directories if necessary
-  sudo mkdir -p /usr/local/share/ca-certificates/luxoft
-  sudo mkdir -p ~/.minikube/files/etc/ssl/certs
-
-  # Generate certificate
-  echo -n | openssl s_client -showcerts -connect dl.k8s.io:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > kube.chain.pem
-
-  # Extract the last certificate
-  csplit -f htf kube.chain.pem '/-----BEGIN CERTIFICATE-----/' '{*}' | sudo cat $(ls htf* | sort | tail -1) > luxoft_root_ca.crt && rm -rf htf*
-
+if [ -n "$LUXUSER" ]; then
   # Copy certifiates
   sudo cp luxoft_root_ca.crt /usr/local/share/ca-certificates/luxoft/luxoft_root_ca.crt
   sudo cp luxoft_root_ca.crt ~/.minikube/files/etc/ssl/certs/luxoft_root_ca.crt
@@ -73,15 +82,5 @@ then
   # Update certificates
   sudo update-ca-certificates
 fi
-
-
-# Install Kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-
-# Give Kubectl execute rights
-chmod +x ./kubectl
-
-# Move kubectl binary to path
-sudo mv ./kubectl /usr/local/bin/kubectl
 
 sudo minikube start --vm-driver=none
